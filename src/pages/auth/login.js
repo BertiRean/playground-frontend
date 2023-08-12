17bf59ce2ed800eb22b00256fc657e446163a707
BertiRean <tplaza15@gmail.com>
2023-08-12 20:17:14 -0300

    Pages: Login - Removed phone method login

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -122,10 +122,6 @@ const Page = () => {
                 label="Email"
                 value="email"
               />
-              <Tab
-                label="Phone Number"
-                value="phoneNumber"
-              />
             </Tabs>
             {method === 'email' && (
               <form
@@ -195,19 +191,6 @@ const Page = () => {
                   </div>
                 </Alert>
               </form>
-            )}
-            {method === 'phoneNumber' && (
-              <div>
-                <Typography
-                  sx={{ mb: 1 }}
-                  variant="h6"
-                >
-                  Not available in the demo
-                </Typography>
-                <Typography color="text.secondary">
-                  To prevent unnecessary costs we disabled this feature in the demo.
-                </Typography>
-              </div>
             )}
           </div>
         </Box>
