7526a30b9bf1bc3b782da58f2f647255054d57b9
BertiRean <tplaza15@gmail.com>
2023-08-13 21:10:32 -0300

    Config: Axios - Added function to set token

--- src/lib/http/client/axios.client.js
+++ src/lib/http/client/axios.client.js
@@ -1,3 +1,13 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "http://127.0.0.1:8000";
+
+const axiosInstance = axios.create({
+  timeout : 2000,
+})
+
+export function setClientToken(token){
+  axios.defaults.headers.common = {
+    'Authorization' : `"Bearer ${token}"`
+  }
+}
