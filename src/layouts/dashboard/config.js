ab4ade1f6890d61ffc2654bfce54cc47cde0e175
BertiRean <tplaza15@gmail.com>
2023-08-15 19:16:33 -0300

    Layout: Changed Home Dashboard

--- src/layouts/dashboard/config.js
+++ src/layouts/dashboard/config.js
@@ -1,4 +1,4 @@
-import ChartBarIcon from '@heroicons/react/24/solid/ChartBarIcon';
+import HomeIcon from '@heroicons/react/24/solid/HomeIcon';
 import CogIcon from '@heroicons/react/24/solid/CogIcon';
 import LockClosedIcon from '@heroicons/react/24/solid/LockClosedIcon';
 import ShoppingBagIcon from '@heroicons/react/24/solid/ShoppingBagIcon';
@@ -11,11 +11,11 @@ import { SvgIcon } from '@mui/material';
 
 export const items = [
   {
-    title: 'Overview',
+    title: 'Dashboard',
     path: '/',
     icon: (
       <SvgIcon fontSize="small">
-        <ChartBarIcon />
+        <HomeIcon />
       </SvgIcon>
     )
   },
