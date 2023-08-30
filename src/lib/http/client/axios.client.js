77602df2160650e4925ca2f9b67aa30e61f3cfc6
BertiRean <tplaza15@gmail.com>
2023-08-30 17:42:13 -0300

    Libs: Axios - Now use env variable for baseUrl

--- src/lib/http/client/axios.client.js
+++ src/lib/http/client/axios.client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000";
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL
 
 const axiosInstance = axios.create({
   timeout : 2000,
