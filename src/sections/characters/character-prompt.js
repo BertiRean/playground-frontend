e9c90f5956fb120e41196405f354068b47dad6b7
BertiRean <tplaza15@gmail.com>
2023-08-31 14:42:32 -0300

    Pages: Voice PLayer improved controls display

--- src/sections/characters/character-prompt.js
+++ src/sections/characters/character-prompt.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import {
   Button,
   Card,
@@ -20,25 +20,25 @@ import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import ReplayIcon from '@mui/icons-material/Replay';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import RecordVoiceOverIcon from '@mui/icons-material/RecordVoiceOver';
 import { useFormik } from 'formik';
 
 export const CharacterPrompt = (props) => {
-  const { character, handleGenDialogue, voices, handlePlayDialogue} = props;
+  const { character, handleGenDialogue, voices, handlePlayDialogue } = props;
 
   const [lines, setLines] = useState([]);
 
   const formik = useFormik({
-    initialValues : {
-      model : 10,
-      voice : 10,
-      dialogues : 2,
+    initialValues: {
+      model: 10,
+      voice: 10,
+      dialogues: 2,
     },
-    onSubmit : async (values, helpers) => {
+    onSubmit: async (values, helpers) => {
       try {
         const response = await handleGenDialogue(character._id, "openai", values.dialogues);
         const new_lines = response.data.lines.map((item, idx) => {
-          return {line : item, audio : ''}
+          return { line: item, audio: '' }
         });
 
         setLines(new_lines);
@@ -68,16 +68,25 @@ export const CharacterPrompt = (props) => {
     setLines(new_lines);
   }
 
-  const Reply = ({ lineIdx, audioUrl="", text = "", voice_name = "", handlePlayDialogue, saveAudioFn = saveAudio, props }) => {
+  const Reply = ({ lineIdx, audioUrl = "", text = "", voice_name = "", handlePlayDialogue, saveAudioFn = saveAudio, props }) => {
+
+    const audioRef = useRef(null);
 
     const onPlay = async (event) => {
-      const option = voices.find((itr) => itr.name === voice_name);
-      if (option){
-        try {
-        const data = await handlePlayDialogue(option.id, text);
-        saveAudioFn(lineIdx, data.url);
-        } catch (error) {
-          console.log(error);
+      if (audioRef !== null && audioRef.current !== null && audioUrl !== "") {
+        audioRef.current.pause();
+        audioRef.current.currentTime = 0;
+        audioRef.current.play();
+      }
+      else {
+        const option = voices.find((itr) => itr.name === voice_name);
+        if (option) {
+          try {
+            const data = await handlePlayDialogue(option.id, text);
+            saveAudioFn(lineIdx, data.url);
+          } catch (error) {
+            console.log(error);
+          }
         }
       }
     }
@@ -90,10 +99,12 @@ export const CharacterPrompt = (props) => {
           <ThumbUpIcon></ThumbUpIcon>
           <ThumbDownIcon></ThumbDownIcon>
           <ReplayIcon></ReplayIcon>
-          <PlayArrowIcon onClick={onPlay}></PlayArrowIcon>
-          <audio src={audioUrl} controls>
-          </audio>
+          <RecordVoiceOverIcon onClick={onPlay}></RecordVoiceOverIcon>
         </Stack>
+        {
+          <audio src={audioUrl} ref={audioRef} controls>
+          </audio>
+        }
       </Box>
     )
   };
@@ -112,7 +123,7 @@ export const CharacterPrompt = (props) => {
               <Stack direction='column' spacing={3}>
                 <Avatar
                   sx={{
-                    alignSelf : 'center',
+                    alignSelf: 'center',
                     width: 328,
                     height: 328,
                   }}
@@ -212,7 +223,7 @@ export const CharacterPrompt = (props) => {
 
 CharacterPrompt.propTypes = {
   character: PropTypes.object.isRequired,
-  handleGenDialogue : PropTypes.func,
-  voices : PropTypes.array,
-  handlePlayDialogue : PropTypes.func,
+  handleGenDialogue: PropTypes.func,
+  voices: PropTypes.array,
+  handlePlayDialogue: PropTypes.func,
 };
