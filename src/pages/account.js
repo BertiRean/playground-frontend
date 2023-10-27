5a63a73cdc0e45a04be17de5eba998148a618cce
BertiRean <tplaza15@gmail.com>
2023-10-27 17:56:31 -0300

    Account: Removed bad import

--- src/pages/account.js
+++ src/pages/account.js
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import { Box, Container, Stack, Typography, Unstable_Grid2 as Grid } from '@mui/material';
 import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
-import { AccountProfile } from 'src/sections/account/account-profile';
 import { AccountProfileDetails } from 'src/sections/account/account-profile-details';
 import { useCookies } from 'react-cookie';
 
